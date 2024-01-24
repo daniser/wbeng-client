be8206bbb46891ff8825d8a25ec32aacb083f6f4
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-01-24 17:06:47 +0200

    Remove custom switchable SerializedPath implementation

--- src/Serializer.php
+++ src/Serializer.php
@@ -6,7 +6,6 @@ namespace TTBooking\WBEngine;
 
 use JMS\Serializer\SerializerInterface as JMSSerializerInterface;
 use Symfony\Component\Serializer\SerializerInterface as SymfonySerializerInterface;
-use TTBooking\WBEngine\Attributes\SerializedPath;
 
 class Serializer implements SerializerInterface
 {
@@ -14,26 +13,12 @@ class Serializer implements SerializerInterface
 
     public function serialize(mixed $data, array $context = []): string
     {
-        static::prepare($context);
-
         return $this->serializer->serialize($data, 'json');
     }
 
     public function deserialize(string $data, string $type, array $context = []): object
     {
-        static::prepare($context);
-
         // @phpstan-ignore-next-line
         return $this->serializer->deserialize($data, $type, 'json');
-    }
-
-    /**
-     * @param array<string, mixed> $context
-     */
-    protected function prepare(array $context): void
-    {
-        if ($this->serializer instanceof SymfonySerializerInterface) {
-            SerializedPath::setMode(true === ($context['legacy'] ?? false) ? 'legacy' : null);
-        }
     }
 }
