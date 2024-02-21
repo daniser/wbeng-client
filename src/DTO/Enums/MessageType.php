aed2c0c84faf051dcc44bfd2146499786b511869
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-02-21 10:40:28 +0200

    MessageType styling

--- src/DTO/Enums/MessageType.php
+++ src/DTO/Enums/MessageType.php
@@ -6,22 +6,22 @@ namespace TTBooking\WBEngine\DTO\Enums;
 
 enum MessageType: string
 {
+    case Debug = 'DEBUG';
     case Info = 'INFO';
     case Notice = 'NOTICE';
     case Warning = 'WARNING';
     case Error = 'ERROR';
-    case Debug = 'DEBUG';
     case ForUser = 'FORUSER';
 
     public function style(): string
     {
         return match ($this) {
-            self::Info => 'bg=cyan;fg=black',
+            self::Debug => 'bg=magenta',
+            self::Info => 'bg=blue',
             self::Notice => 'bg=green;fg=black',
             self::Warning => 'bg=yellow;fg=black',
             self::Error => 'error',
-            self::Debug => 'bg=magenta',
-            self::ForUser => 'bg=cyan;fg=white',
+            self::ForUser => 'bg=cyan;fg=black',
         };
     }
 }
